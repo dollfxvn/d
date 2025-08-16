bcea0550d88530bdfa100fa2b98f125052faf7a1
dollfxvn <dollfxvnbeta@gmail.com>
2025-08-16 12:01:36 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,1 +1,9 @@
-
+addEventHandler("onPlayerWasted", root, function(_, killer, weapon, bodypart)
+    if killer and weapon == 9 then
+        if getElementType(killer) == "player" then
+			for _, player in ipairs(getElementsByType("player")) do
+				exports.infobox:addBox(player, "info", getPlayerName(source):gsub("_", " ").." adlı oyuncu "..getPlayerName(killer):gsub("_", " ").." oyuncu tarafından testere ile katletilmiştir.")
+			end
+		end
+    end
+end)
